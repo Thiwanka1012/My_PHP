8129a48730745e21f274ecdedee1076ee338497f
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-02-12 17:20:38 +0530

    Initial commit

---
+++ my-first-phpfile.php
@@ -0,0 +1,22 @@
+<?php 
+
+
+		//This is oneLine comment
+		echo "This is my php file."; 
+
+		echo "<br>";
+		echo "ADoo. ";
+
+		echo "<br>";
+
+		/* This is Multiline commnet*/
+
+		echo "First Name" . "Second name";
+		echo "<br>";
+
+
+		#This is another page
+		echo 6+10;
+
+
+?>
