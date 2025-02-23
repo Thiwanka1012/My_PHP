eeb4c4549290cf043aef2c6ca9b643420670aae3
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-02-23 10:54:23 +0530

    Floating Points

---
+++ floating_point.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Floating Points</title>
+</head>
+<body>
+
+	<h2>Floating Point</h2>
+
+
+	<?php 
+
+		$float = 6.28;
+
+
+
+
+	 ?>
+
+	 <?php echo $float; ?>
+
+
+	 <br>
+
+
+	 <?php $float+=5; ?>
+	 <?php echo $float; ?>
+
+</body>
+</html>
