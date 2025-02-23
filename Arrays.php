abd3c0840bfbb5111b913901b230c2350a798597
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-02-23 12:51:02 +0530

    Arrays1

---
+++ Arrays.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Arrays</title>
+</head>
+<body>
+
+	<?php 
+		$mycars= array("Toyota","Nissan","Kia","Mazda");
+		echo $mycars[3];
+
+	 ?>
+
+	 <br>
+
+	 <pre>
+	 <?php print_r($mycars); ?>
+	 </pre>
+
+</body>
+</html>
