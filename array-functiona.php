e6ddaba3e8c1fce7ab0bd3de0620c3b159e933d8
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-02-23 17:59:20 +0530

    Array  Functions

---
+++ array-functiona.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Array Functions</title>
+</head>
+<body>
+
+<?php 
+
+	$mynumbers = array(14,26,78,99,00);
+
+
+
+ ?>
+
+
+ Maximum value: <?php echo max($mynumbers); ?><br>
+ Minimum Value:<?php echo min($mynumbers) ?><br>
+ Count: <?php echo count($mynumbers) ?><br>
+</body>
+</html>
