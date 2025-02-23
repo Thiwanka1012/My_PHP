0761cb06efd83a486c7bc74eecf8b7daff40c0a6
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-02-23 16:56:10 +0530

    Assosiate arrays

---
+++ assosiative-arrays.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Assosiative arrays</title>
+</head>
+<body>
+
+<?php 
+
+	$student=array("first_name"=>"Deleep","last_name"=>"Kariyawasam","age"=>19);
+
+
+ ?>
+
+
+ <?php echo $student["first_name"]; ?>
+</body>
+</html>
