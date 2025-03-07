d4235a7a99d0f3fd7d977dbace2a77bfc74d66b1
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-07 20:56:12 +0530

    updated

--- floating_point.php
+++ floating_point.php
@@ -31,6 +31,27 @@
 
 	 Round: <?php echo round($float,3); ?><br>
 	 Round up: <?php echo ceil($float); ?><br>
+	 Round Down: <?php echo floor($float); ?><br>
+
+
+	 <h2>How to check if a number is an integer or a float</h2>
+
+
+	 <?php 
+
+	 	$myinteger=33;
+	 	$myfloat=55.555;
+
+
+
+
+
+	  ?>
+
+
+
+	  <?php echo "Is {myfloat} an float?" . is_float($myfloat); ?><br>
+	  <?php echo "Is {myinteger} an integer?" . is_integer($myinteger); ?><br>
 
 
 
