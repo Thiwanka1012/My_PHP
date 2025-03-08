94035af882217b7db8bef6f4e061a099cd7deb31
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-09 01:04:46 +0530

    for loops 1

---
+++ for-loops.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>For loops</title>
+</head>
+<body>
+	<?php 
+
+	for ($i=0; $i <= 12 ; $i++) { 
+		echo "12 * {$i} = " . 12*$i . "<br>";
+	}
+
+	 ?>
+
+</body>
+</html>
