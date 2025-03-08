edb7eac420786845093d01379de7e3a0bc30f2b5
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-08 17:46:06 +0530

    Boolean Values

---
+++ boolean.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Boolean</title>
+</head>
+<body>
+
+	<?php 
+
+		$var1 = true;
+		$var2 = false;
+
+
+
+
+
+
+	 ?>
+
+
+
+	 <?php echo "Variable 1:" . $var1; ?> <br>
+	 <?php echo "Variable 2:" . $var2; ?><br>
+</body>
+</html>
