fd16975121a334f2435151aeecf846d696f8151f
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-08 18:25:57 +0530

    If-statement

---
+++ if-statement.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>If statement</title>
+</head>
+
+<body>
+
+<?php 
+
+	$avg_score =50;
+
+	if ($avg_score > 40){
+		echo "You are Qualified";
+	}
+
+	else{
+		echo "You are Not Qualified";
+	}
+
+
+ ?>
+
+</body>
+</html>
