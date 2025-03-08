4a38e1865ed595991b0f90228ec1df0e1df9de63
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-09 00:10:55 +0530

    Logical Operators

---
+++ logical-operators.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Logical Operators</title>
+</head>
+<body>
+
+	<?php 
+
+		$subject1= 60;
+		$subject2= 90;
+
+
+		echo "Subject 1:" . $subject1 . "<br>";
+		echo "Subject 2:" . $subject2 . "<br>";
+
+
+		if (($subject1 > 50) && ($subject2 > 50)) {
+
+			echo "You have passed the exam";
+		}
+
+		else{
+			echo "You need to pass both subjects";
+		}
+
+
+	 ?>
+
+</body>
+</html>
