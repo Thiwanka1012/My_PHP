528193f65a126970d808bf68e4d61dc5fd651e35
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-09 00:50:50 +0530

    while loop

---
+++ while-loop.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>While Loop</title>
+</head>
+<body>
+
+	<?php 
+
+		$i=0;
+
+		while ($i <=10 ) {
+			echo $i . "<br>";
+			$i++;
+		}
+
+
+
+
+	 ?>
+
+</body>
+</html>
