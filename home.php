d45da37e7dfa7c4e8f810100a533ecb75d0f11f7
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-09 10:55:51 +0530

    ss

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home page</title>
+</head>
+<body>
+
+	<h1>Home Page</h1>
+
+
+	<a href="news.php?news_item=32&category=sports">Read News Item 1</a>
+
+</body>
+</html>
---
+++ news.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>News</title>
+</head>
+<body>
+
+	<h1>News Page</h1>
+
+	<pre>	<?php 
+
+		print_r($_GET);
+
+
+	 ?>
+	 </pre>
+
+
+</body>
+</html>
