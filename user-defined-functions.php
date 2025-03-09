e48a9f2a8bb998fd840ae202254f82c93c029756
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-09 09:57:20 +0530

    Wrte value

--- user-defined-functions.php
+++ user-defined-functions.php
@@ -9,32 +9,34 @@
 
 	<?php 
 
-		function get_month_name($monthvalue){
+		function get_month_name($monthvalue=0 , $yearvalue=2016){
 
 
 			switch ($monthvalue) {
-				case 1: echo "Duruthu";break;
-				case 2: echo "Nawam";break;
-				case 3: echo "Medin";break;
-				case 4: echo "Bak";break;
-				case 5: echo "Vesak";break;
-				case 6: echo "Poson";break;
-				case 7: echo "Esala";break;
-				case 8: echo "Nikini";break;
-				case 9: echo "Binara";break;
-				case 10: echo "Vap";break;
-				case 11: echo "Ill";break;
-				case 12: echo "Uduwap";break;
-				default: echo "Invalid Month";break;
+				case 1: $monthname="Duruthu";break;
+				case 2: $monthname="Nawam";break;
+				case 3: $monthname="Medin";break;
+				case 4: $monthname="Bak";break;
+				case 5: $monthname="Vesak";break;
+				case 6: $monthname="Poson";break;
+				case 7: $monthname="Esala";break;
+				case 8: $monthname="Nikini";break;
+				case 9: $monthname="Binara";break;
+				case 10: $monthname="Vap";break;
+				case 11: $monthname="Ill";break;
+				case 12: $monthname="Uduwap";break;
+				default: $monthname="Invalid Month";break;
 			}
 
 			
-
+			return $yearvalue . " " . $monthname;
 
 
 		}
 
-		echo get_month_name(8);
+		echo get_month_name(8,2014);
+		echo "<br>";
+		echo get_month_name(1,2025);
 
 
 
